536650bb3de3f0019b17c19c29506cfc259ac2c9
Dave Cleaver <dscleaver@gmail.com>
2012-05-06 21:43:49 -0400

    updating parser to parse the empty list

--- scheem/parser.js
+++ scheem/parser.js
@@ -371,40 +371,82 @@ var parseScheem = (function(){
               }
             }
             if (result0 !== null) {
-              result1 = [];
-              result2 = parse_whitespace();
-              while (result2 !== null) {
-                result1.push(result2);
-                result2 = parse_whitespace();
+              if (input.charCodeAt(pos) === 41) {
+                result1 = ")";
+                pos++;
+              } else {
+                result1 = null;
+                if (reportFailures === 0) {
+                  matchFailed("\")\"");
+                }
               }
               if (result1 !== null) {
-                result2 = parse_expression();
-                if (result2 !== null) {
-                  result3 = [];
-                  result4 = parse_spaceexpression();
-                  while (result4 !== null) {
-                    result3.push(result4);
+                result0 = [result0, result1];
+              } else {
+                result0 = null;
+                pos = pos1;
+              }
+            } else {
+              result0 = null;
+              pos = pos1;
+            }
+            if (result0 !== null) {
+              result0 = (function(offset) { return []; })(pos0);
+            }
+            if (result0 === null) {
+              pos = pos0;
+            }
+            if (result0 === null) {
+              pos0 = pos;
+              pos1 = pos;
+              if (input.charCodeAt(pos) === 40) {
+                result0 = "(";
+                pos++;
+              } else {
+                result0 = null;
+                if (reportFailures === 0) {
+                  matchFailed("\"(\"");
+                }
+              }
+              if (result0 !== null) {
+                result1 = [];
+                result2 = parse_whitespace();
+                while (result2 !== null) {
+                  result1.push(result2);
+                  result2 = parse_whitespace();
+                }
+                if (result1 !== null) {
+                  result2 = parse_expression();
+                  if (result2 !== null) {
+                    result3 = [];
                     result4 = parse_spaceexpression();
-                  }
-                  if (result3 !== null) {
-                    result4 = [];
-                    result5 = parse_whitespace();
-                    while (result5 !== null) {
-                      result4.push(result5);
-                      result5 = parse_whitespace();
+                    while (result4 !== null) {
+                      result3.push(result4);
+                      result4 = parse_spaceexpression();
                     }
-                    if (result4 !== null) {
-                      if (input.charCodeAt(pos) === 41) {
-                        result5 = ")";
-                        pos++;
-                      } else {
-                        result5 = null;
-                        if (reportFailures === 0) {
-                          matchFailed("\")\"");
-                        }
+                    if (result3 !== null) {
+                      result4 = [];
+                      result5 = parse_whitespace();
+                      while (result5 !== null) {
+                        result4.push(result5);
+                        result5 = parse_whitespace();
                       }
-                      if (result5 !== null) {
-                        result0 = [result0, result1, result2, result3, result4, result5];
+                      if (result4 !== null) {
+                        if (input.charCodeAt(pos) === 41) {
+                          result5 = ")";
+                          pos++;
+                        } else {
+                          result5 = null;
+                          if (reportFailures === 0) {
+                            matchFailed("\")\"");
+                          }
+                        }
+                        if (result5 !== null) {
+                          result0 = [result0, result1, result2, result3, result4, result5];
+                        } else {
+                          result0 = null;
+                          pos = pos1;
+                        }
                       } else {
                         result0 = null;
                         pos = pos1;
@@ -425,15 +467,12 @@ var parseScheem = (function(){
                 result0 = null;
                 pos = pos1;
               }
-            } else {
-              result0 = null;
-              pos = pos1;
-            }
-            if (result0 !== null) {
-              result0 = (function(offset, first, rest) { return [ first ].concat(rest); })(pos0, result0[2], result0[3]);
-            }
-            if (result0 === null) {
-              pos = pos0;
+              if (result0 !== null) {
+                result0 = (function(offset, first, rest) { return [ first ].concat(rest); })(pos0, result0[2], result0[3]);
+              }
+              if (result0 === null) {
+                pos = pos0;
+              }
             }
           }
         }
